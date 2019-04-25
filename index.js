7d5187e1d1f86cda963bbca6d9753895bf811417
unknown <ricardo.queiros@gmail.com>
2019-04-25 16:32:59 +0100

    Endpoints

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ btnSearch.addEventListener("click", function () {
                 console.log(doc)
             }
         });
-        renderTable(objs)
+        renderApis(objs)
     });
 })
 
@@ -20,10 +20,13 @@ btnSearch.addEventListener("click", function () {
  * 
  * @param {Array } objs 
  */
-const renderTable = (objs) => {
-    const table = document.querySelector("table")
+const renderApis = objs => {
+    const table = document.querySelector("#tblApis")
     if (objs.length != 0) {
-        table.innerHTML = `        
+        table.innerHTML = `
+        <thead class="thead-dark">
+            <tr><th colspan="4">1. Select an API</th></tr>    
+            </thead>    
             <tr>                
                 <th scope="col">#</th>
                 <th scope="col">Description</th>
@@ -41,20 +44,21 @@ const renderTable = (objs) => {
                     <td>${objApi.description}</td>
                     <td>${objApi.base_url}</td>                
                     <td>
-                        <button id='${idApi}' class='btn btn-danger endpoints'>get endpoints</button>  
+                        <button id='${idApi}' class='btn btn-danger apis'>get endpoints</button>  
                     </td>  
                 </tr>
             `
             i++
         }
 
-        const endpoints = document.getElementsByClassName("endpoints")
+        const endpoints = document.getElementsByClassName("apis")
         for (const endpoint of endpoints) {
             endpoint.addEventListener("click", function () {
                 const docRef = db.collection("apis").doc(this.id)
                 docRef.get().then(function (doc) {
                     if (doc.exists) {
                         const apiEndpoints = doc.data().endpoints
+                        renderEndpoints(apiEndpoints)
                         for (const apiEndpoint of apiEndpoints) {
                             console.log(apiEndpoint)
                         }
@@ -72,5 +76,79 @@ const renderTable = (objs) => {
         table.innerHTML = ""
     }
 
+
+}
+
+/**
+ * 
+ * @param {Array } endpoints of a specific API 
+ */
+const renderEndpoints = endpoints => {
+    const table = document.querySelector("#tblEndpoints")
+    if (endpoints.length != 0) {
+        table.innerHTML = `
+        <thead class="thead-dark">
+            <tr><th colspan="4">2. Select an Endpoint</th></tr>        
+        </thead>
+            <tr>                
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Endpoint</th>
+                <th scope="col">Actions</th>
+            </tr>
+        `
+        let i = 0
+        for (const endpoint of endpoints) {
+            table.innerHTML += `
+                <tr>
+                    <td>${i+1}</td>
+                    <td>${endpoint.description}</td>
+                    <td>${endpoint.endpoint}</td>                
+                    <td>
+                        <button id='${i+1}' class='btn btn-danger endpoints'>run endpoint</button>  
+                    </td>  
+                </tr>
+            `
+            i++
+        }
+        const btnEndpoints = document.getElementsByClassName("endpoints")
+        for (const btnEndpoint of btnEndpoints) {
+            btnEndpoint.addEventListener("click", function () {
+                const placeholders = endpoints[this.id - 1].placeholders
+                let endpoint = endpoints[this.id - 1].endpoint
+                if (placeholders.length) {
+                    for (const placeholder of placeholders) {
+                        const response = prompt(placeholder.name + "?")
+                        console.log("Response: " + response)
+                        console.log("Id: " + placeholder.id)
+                        endpoint = endpoint.replace(placeholder.id, response)
+                    }
+                    renderLanguages(endpoint)
+                }
+            })
+        }
+    } else {
+        table.innerHTML = ""
+    }
+
+
+}
+
+/**
+ * 
+ * @param {Array } endpoints of a specific API 
+ */
+const renderLanguages = endpoint => {
+    const table = document.querySelector("#tblLanguages")
+    table.style.visibility = "visible"
+    const txtRequestCode = document.querySelector("#txtRequestCode")
+    txtRequestCode.value = `
+        let response = await fetch('${endpoint}');
+        let data = await response.json();
+        console.log(data);    
+    `
+
+    const btnRun = document.querySelector("#btnRun")
+    btnRun.addEventListener("")
 
 }
